c6ab097ca6903dfc224ce0d2589b75121076f7fc
cherubalex <123826150+cherubalex@users.noreply.github.com>
2023-01-28 20:02:01 +0100

    commit 2

--- app/src/main/java/com/example/ez_docs_app/MainActivity.kt
+++ app/src/main/java/com/example/ez_docs_app/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.ez_docs_app
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ez_docs_app.ui.theme.EZ_Docs_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+    val configuration = LocalConfiguration.current
+    val screenwidth = configuration.screenWidthDp
+    val screenpadding = 100
+    Column() {
+        Text(text = "Hello $name!")
+        Box(
+            modifier = Modifier
+                .padding((screenpadding/2).dp)
+                .clip(CircleShape)
+                .width((screenwidth - 30).dp)
+                .height(40.dp)
+                .background(Color.Blue)
+
+        )
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
